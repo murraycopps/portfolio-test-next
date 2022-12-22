8e5ed9d99ce6256a430cf7c8fbe5a8128f8a696c
murraycopps <murraycopps@gmail.com>
2022-12-22 11:38:36 -0500

    reworded about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,10 +5,10 @@ const About = () => (
     <PageWrapper title="About Me">
         <h1 className="text-3xl font-bold leading-tight text-center text-gray-300 mb-4">About Me</h1>
         <p className="text-lg font-medium leading-tight text-center text-gray-400 mt-2">
-            I am a high school student with a passion for coding and engineering. I have always been fascinated by the way computers work, and I love to explore new technologies and techniques. I have participated in several hackathons and science fairs, and I am always looking for new opportunities to learn and grow.
+            As a high school student with a deep-seated passion for coding and engineering, I have always been fascinated by the inner workings of computers and the endless possibilities of technology. I love to delve into new technologies and techniques, constantly seeking out opportunities to learn and grow as a programmer. In my free time, I enjoy building projects and experimenting with different programming languages and frameworks, constantly striving to improve my skills and knowledge. Whether it's through online tutorials or other learning resources, I am always eager to take on new challenges and push myself to new heights in the field.
             <br />
             <br />
-            In my free time, I enjoy building projects and experimenting with different programming languages and frameworks. I have experience with languages such as JavaScript, Python, and C++, and I am always looking for ways to improve my skills and knowledge.
+            In my free time, I enjoy building projects and experimenting with different JavaScript frameworks. I have experience with frameworks such as React, React Native, and Next.js, and I also enjoy making computer games using vanilla JavaScript, CSS, and HTML. I am always looking for ways to improve my skills and knowledge in front-end development and game development.
             <br />
             <br />
             Thank you for visiting my portfolio. I hope you find it interesting and informative, and I look forward to hearing from you.
